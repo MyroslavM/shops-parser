85581bf9ecaa4177bea09c243ba9cb15ce78bef0
vlad <pavliuk.vlad@gmail.com>
2018-04-30 19:35:17 +0300

    Added search page.

---
+++ resources/views/search.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+
+@section('content')
+    <div>
+        <form method="GET" action="">
+            <input type="text">
+            <input type="submit" value="search">
+        </form>
+
+    </div>
+@endsection
