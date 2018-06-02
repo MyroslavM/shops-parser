3f6372c176ee5bcd79ee16f2663787fef81a1625
vlad <pavliuk.vlad@gmail.com>
2018-06-02 22:38:53 +0300

    add products image

--- resources/views/prodacts.blade.php
+++ resources/views/prodacts.blade.php
@@ -1,18 +1,23 @@
 @extends('layout.main')
 
 @section('content')
+
     @foreach($countries as $countryTitle => $products)
         <h2>{{ $countryTitle }}</h2>
-
+        
         @if(sizeof($products) > 0)
             <table border="1">
                 <tr>
+                    <th>Image</th>
                     <th>Title</th>
                     <th>Price</th>
                     <th>Link</th>
                 </tr>
                 @foreach($products as $product)
                 <tr>
+                    <td>
+                        <img src="{{ $product['img'] }}" alt="{{$product['title']}}" height="160">
+                    </td>
                     <td>
                         {{$product['title']}}
                     </td>
